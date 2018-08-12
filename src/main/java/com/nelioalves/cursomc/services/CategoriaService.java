324345d1bd617198b803b28c05fafb41d2491fc3
Lucas de Carvalho Lopes <lucas.dc.lopes@gmail.com>
2018-08-12 16:07:07 -0300

    Criando repository e service para a categoria

---
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nelioalves.cursomc.services;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author Lucas
+ */
+@Service    
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository repo;
+    
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
